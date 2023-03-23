f5a30dba6c093b851044720b6a618fd86496e766
h2magic <h2magic@126.com>
2023-03-23 22:38:09 +0800

    finish load localization

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:super_continent_client/pages/home_page.dart';
+import 'package:super_continent_client/utils/localization/core.dart';
+import 'package:super_continent_client/utils/reference.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    String t = MyLocalization.get("title", fmt: {"buf": " 2023-12-03"});
     return GetMaterialApp(
-      title: "",
+      title: "XXX",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(
+        title: t,
+      ),
     );
   }
 }
-
-
--- lib/utils/localization/core.dart
+++ lib/utils/localization/core.dart
@@ -1,24 +1,37 @@
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:super_continent_client/utils/reference.dart';
 
-import '../logger.dart';
-
-class Localization {
-  static late String stringJson;
+class MyLocalization {
+  static late String _stringJson;
+  static late Map _wordMap;
 
   static loadJsonFile(String language) async {
-    final jsonString =
-        await rootBundle.loadString("assets/localization/$language.json");
+    final jsonString = await rootBundle.loadString(
+      "assets/localization/$language.json",
+    );
 
     return jsonString;
   }
 
   static initial({String language = "en"}) async {
     try {
-      stringJson = await loadJsonFile(language);
+      _stringJson = await loadJsonFile(language);
     } catch (e) {
-      logE(e.toString());
-      stringJson = await loadJsonFile("en");
+      printf(e);
+      _stringJson = await loadJsonFile("en");
     }
+    _wordMap = jsonDecode(_stringJson);
+  }
+
+  static String get(String key, {Map<String, String>? fmt}) {
+    String template = _wordMap[key]!;
+    if (fmt == null) {
+      return template;
+    }
+    for (String f in fmt.keys) {
+      template = template.replaceAll("{{$f}}", fmt[f]!);
+    }
+    return template;
   }
 }
