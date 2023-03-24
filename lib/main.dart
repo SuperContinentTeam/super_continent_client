4620aafc51fa7eedf8b820ef77322c375caed91f
WY <tangwenyun@esoaru.com>
2023-03-24 11:46:55 +0800

    修改状态栏

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:super_continent_client/root_page.dart';
 import 'package:super_continent_client/storage/kv/core.dart';
 import 'package:super_continent_client/utils/localization/core.dart';
@@ -7,5 +8,8 @@ main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HiveDataBase.initial();
   await MyLocalization.initial(language: "cn");
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ),);
   runApp(const RootPage());
 }
