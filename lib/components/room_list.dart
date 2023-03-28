7ef0b11703d23d742f100e17d3c5b132a54dd1cd
h2magic <h2magic@126.com>
2023-03-28 16:40:49 +0800

    finish table for room

--- lib/components/room_list.dart
+++ lib/components/room_list.dart
@@ -10,27 +10,28 @@ class RoomList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Table(
-      border: TableBorder.all(),
-      children: [
-        _buildHeader(),
-      ],
-      columnWidths: const {
-        0: FractionColumnWidth(0.1),
-        1: FractionColumnWidth(0.5),
-        2: FractionColumnWidth(0.2),
-        3: FractionColumnWidth(0.1),
-        4: FractionColumnWidth(0.1),
-      },
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Table(
+        border: TableBorder.all(),
+        children: [_buildHeader(), ..._buildBody()],
+        columnWidths: const {
+          0: FractionColumnWidth(0.05),
+          1: FractionColumnWidth(0.55),
+          2: FractionColumnWidth(0.1),
+          3: FractionColumnWidth(0.1),
+          4: FractionColumnWidth(0.1),
+        },
+      ),
     );
   }
 
-  _buildCell(String content) {
+  _buildCell(String content, {Color? color = Colors.black}) {
     return TableCell(
       child: Container(
         alignment: Alignment.center,
         padding: const EdgeInsets.all(10.0),
-        child: Text(content),
+        child: Text(content, style: TextStyle(color: color)),
       ),
     );
   }
@@ -48,5 +49,36 @@ class RoomList extends StatelessWidget {
     );
   }
 
-  _buildTable() {}
+  _buildBody() {
+    return roomList
+        .asMap()
+        .entries
+        .map((e) => TableRow(children: [
+              _buildCell(e.key.toString()),
+              _buildCell(e.value.name),
+              _buildCell(
+                roomStatus[e.value.status].toString(),
+                color: e.value.status == 0 ? Colors.green : Colors.red,
+              ),
+              _buildCell("${e.value.currentNumber}/${e.value.limitNumber}"),
+              TableCell(
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      MyLocalization.get(
+                        "room-action-${e.value.status == 0 ? 'join' : 'watch'}",
+                      ),
+                      style: TextStyle(
+                        color: e.value.status == 0 ? Colors.green : Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ]))
+        .toList();
+  }
 }
--- lib/pages/online_page/controller.dart
+++ lib/pages/online_page/controller.dart
@@ -12,6 +12,16 @@ List<Room> demoRoomList = [
   Room("Test7", 12, 0, "xxx7"),
   Room("Test8", 20, 1, "xxx8"),
   Room("Test9", 12, 0, "xxx9"),
+  Room("Test10", 20, 1, "xxx10"),
+  Room("Test1", 12, 0, "xxx1"),
+  Room("Test2", 20, 1, "xxx2"),
+  Room("Test3", 12, 0, "xxx3"),
+  Room("Test4", 20, 1, "xxx4"),
+  Room("Test5", 12, 0, "xxx5"),
+  Room("Test6", 20, 1, "xxx6"),
+  Room("Test7", 12, 0, "xxx7"),
+  Room("Test8", 20, 1, "xxx8"),
+  Room("Test9", 12, 0, "xxx9"),
   Room("Test10", 20, 1, "xxx10")
 ];
 
