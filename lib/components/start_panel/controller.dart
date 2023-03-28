bf98eb882e7173583afb1216dcad25972947664c
h2magic <h2magic@126.com>
2023-03-28 16:56:13 +0800

    .

--- lib/components/start_panel/controller.dart
+++ lib/components/start_panel/controller.dart
@@ -1,7 +1,8 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:super_continent_client/utils/dialogs/confirm_dialog.dart';
+
+import '../../utils/dialogs/show_color_picker.dart';
 
 class StartPanelController extends GetxController {
   RxInt worldWidth = 10.obs;
